604e14683a889d9ae9bd8e8abab7ce5699988ec8
Volker Theile <volker.theile@openmediavault.org>
2018-01-17 10:56:17 +0100

    Display logo only.
    Signed-off-by: Volker Theile <volker.theile@openmediavault.org>

--- conf.py
+++ conf.py
@@ -96,7 +96,7 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"logo_only": "True"}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.

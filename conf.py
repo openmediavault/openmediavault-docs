f0916a412112cfc7c83990414d886406c8cbae7d
Volker Theile <votdev@gmx.de>
2025-11-23 16:01:46 +0100

    Display `Last updated` on every page
    Signed-off-by: Volker Theile <votdev@gmx.de>
    (cherry picked from commit 98929d079ec3434c23f9a11ae364af43217bd544)
    Signed-off-by: Volker Theile <votdev@gmx.de>

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ import sphinx_fontawesome
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_fontawesome']
+extensions = ['sphinx_fontawesome', 'sphinx_last_updated_by_git']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
